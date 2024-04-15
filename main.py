ab937740ed39b1d0dd3dd78d95b2368c5a8a9c72
Egor <geo-04@mail.ru>
2024-04-15 18:33:00 +0700

    12

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ class Hotel:
         self.double_free = {}
         self.half_luxe_free = {}
         self.luxe_free = {}
+        self.single_full = {}
+        self.double_full = {}
+        self.half_luxe_full = {}
+        self.luxe_full = {}
 
     def read_file(self, hotel_file_name):
         with open(hotel_file_name, 'r', encoding='utf-8') as file:
@@ -15,13 +19,19 @@ class Hotel:
                     line_list.append(itr)
                 if line_list[1] == 'одноместный':
                     self.single_free[line_list[0]] = line_list
+                    self.single_free[line_list[0]].append(2900)
                 if line_list[1] == 'двухместный':
                     self.double_free[line_list[0]] = line_list
+                    self.double_free[line_list[0]].append(2300)
                 if line_list[1] == 'полулюкс':
                     self.half_luxe_free[line_list[0]] = line_list
+                    self.half_luxe_free[line_list[0]].append(3200)
                 if line_list[1] == 'люкс':
                     self.luxe_free[line_list[0]] = line_list
+                    self.luxe_free[line_list[0]].append(4100)
 
 hotel = Hotel()
 
 hotel.read_file(hotel_file_name)
+
+print(hotel.single_free)
