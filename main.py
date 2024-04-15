168e2a63041951e412da4f7b5d04db2da66342e0
Egor <geo-04@mail.ru>
2024-04-15 18:40:54 +0700

    12

--- main.py
+++ main.py
@@ -17,18 +17,42 @@ class Hotel:
                 line_list = []
                 for itr in item.split():
                     line_list.append(itr)
+
                 if line_list[1] == 'одноместный':
                     self.single_free[line_list[0]] = line_list
-                    self.single_free[line_list[0]].append(2900)
+                    if line_list[3] == 'стандарт':
+                        self.single_free[line_list[0]].append(2900)
+                    if line_list[3] == 'стандарт_улучшенный':
+                        self.single_free[line_list[0]].append(3480)
+                    if line_list[3] == 'апартамент':
+                        self.single_free[line_list[0]].append(4350)
+
                 if line_list[1] == 'двухместный':
                     self.double_free[line_list[0]] = line_list
-                    self.double_free[line_list[0]].append(2300)
+                    if line_list[3] == 'стандарт':
+                        self.double_free[line_list[0]].append(2300)
+                    if line_list[3] == 'стандарт_улучшенный':
+                        self.double_free[line_list[0]].append(2760)
+                    if line_list[3] == 'апартамент':
+                        self.double_free[line_list[0]].append(3450)
+
                 if line_list[1] == 'полулюкс':
                     self.half_luxe_free[line_list[0]] = line_list
-                    self.half_luxe_free[line_list[0]].append(3200)
+                    if line_list[3] == 'стандарт':
+                        self.half_luxe_free[line_list[0]].append(3200)
+                    if line_list[3] == 'стандарт_улучшенный':
+                        self.half_luxe_free[line_list[0]].append(3840)
+                    if line_list[3] == 'апартамент':
+                        self.half_luxe_free[line_list[0]].append(4800)
+
                 if line_list[1] == 'люкс':
                     self.luxe_free[line_list[0]] = line_list
-                    self.luxe_free[line_list[0]].append(4100)
+                    if line_list[3] == 'стандарт':
+                        self.luxe_free[line_list[0]].append(4100)
+                    if line_list[3] == 'стандарт_улучшенный':
+                        self.luxe_free[line_list[0]].append(4920)
+                    if line_list[3] == 'апартамент':
+                        self.luxe_free[line_list[0]].append(6150)
 
 hotel = Hotel()
 
