dccfaf47358e5c9453d4632ebe05747a828fca70
Egor <geo-04@mail.ru>
2024-04-15 17:37:46 +0700

    12

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ hotel_file_name = 'fund.txt'
 
 class Hotel:
     def __init__(self):
-        self.single = {}
-        self.double = {}
-        self.half_luxe = {}
-        self.luxe = {}
+        self.single_free = {}
+        self.double_free = {}
+        self.half_luxe_free = {}
+        self.luxe_free = {}
 
     def read_file(self, hotel_file_name):
         with open(hotel_file_name, 'r', encoding='utf-8') as file:
@@ -13,7 +13,14 @@ class Hotel:
                 line_list = []
                 for itr in item.split():
                     line_list.append(itr)
-                print(line_list)
+                if line_list[1] == 'одноместный':
+                    self.single_free[line_list[0]] = line_list
+                if line_list[1] == 'двухместный':
+                    self.double_free[line_list[0]] = line_list
+                if line_list[1] == 'полулюкс':
+                    self.half_luxe_free[line_list[0]] = line_list
+                if line_list[1] == 'люкс':
+                    self.luxe_free[line_list[0]] = line_list
 
 hotel = Hotel()
 
