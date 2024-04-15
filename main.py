3f93d76ad73d73a62d9af69210f33398c67ac63d
GEO-nsk <78921559+GEO-nsk@users.noreply.github.com>
2024-04-15 17:19:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-'popa'
+hotel_file_name = 'fund.txt'
+
+class Hotel:
+    def __init__(self):
+        self.single = {}
+        self.double = {}
+        self.half_luxe = {}
+        self.luxe = {}
+
+    def read_file(self, hotel_file_name):
+        with open(hotel_file_name, 'r', encoding='utf-8') as file:
+            for item in file:
+                line_list = []
+                for itr in item.split():
+                    line_list.append(itr)
+                print(line_list)
+
+hotel = Hotel()
+
+hotel.read_file(hotel_file_name)
